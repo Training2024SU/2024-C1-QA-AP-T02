5df6dab284ed68e2295daba14929741d62c23aa8
David Bonelo <bonelodev@gmail.com>
2024-05-06 10:32:54 -0500

    initial rest-cucumber project

---
+++ api-testing/src/test/java/com/davidbonelo/stepdefinitions/UserSteps.java
@@ -0,0 +1,14 @@
+package com.davidbonelo.stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class UserSteps {
+    @When("a user requests its profile data")
+    public void aUserRequestsItsProfileData() {
+    }
+
+    @Then("he should see his correct information")
+    public void heShouldSeeHisCorrectInformation() {
+    }
+}
---
+++ api-testing/src/test/resources/features/users.feature
@@ -0,0 +1,5 @@
+Feature: Users
+
+  Scenario: User profile
+    When a user requests its profile data
+    Then he should see his correct information
